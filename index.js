45dfd0e7acc68961096546fa0c809320d2e91b78
Tayná Trombin <86115551+TayFrambz@users.noreply.github.com>
2022-05-15 22:26:33 -0300

    Node com express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-require("./modules/http");
+// const { Person } = require("./modules/person");
+
+// require("./modules/path");
+// require("./modules/http");
+
+require('./modules/express');
+
+// const person = new Person("Tay");
--- modules/http.js
+++ modules/http.js
@@ -21,4 +21,4 @@ const server = http.createServer((req, res) => {
     }
 })
 
-server.listen(port, () => console.log(`Rodando na porta ${port}!`));
+server.listen(port, () => console.log(`Rodando na porta ${port}!`)); 
